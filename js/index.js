66003b63aa824d83cd843f3913852fba4631cfc8
Temigo <temigo@gmx.com>
2019-05-07 12:21:39 -0700

    Add second loop for preferences

--- js/index.js
+++ js/index.js
@@ -83,7 +83,7 @@ function make_slides(f) {
 
   slides.one_slider = slide({
     name : "one_slider",
-    present: exp.videos, //every element in exp.stims is passed to present_handle one by one as 'stim'
+    present: exp.videos1, //every element in exp.stims is passed to present_handle one by one as 'stim'
     start: function() {
       this.times = [];
       var player = videojs('experiment-video', {
@@ -169,7 +169,100 @@ function make_slides(f) {
 
     }
   });
+////////////////////////////////////////////////////////////////////////////////
+slides.preference_slide = slide({
+  name : "preference_slide",
+  present: exp.videos2, //every element in exp.stims is passed to present_handle one by one as 'stim'
+  start: function() {
+    this.times = [];
+    var player = videojs('preference-video', {
+      controls: true,
+      autoplay: false,
+      preload: 'auto',
+      inactivityTimeout: 0, // keep control bar visible
+      fluid: true, // center and adapt to video ratio
+      controlBar: {
+        children: [
+          //"playToggle", // remove play button
+          "volumePanel",
+          "volumeMenuButton",
+          "durationDisplay",
+          "timeDivider",
+          "currentTimeDisplay",
+          "progressControl",
+          "remainingTimeDisplay",
+          "fullscreenToggle"
+        ]
+      }
+    }, function() {
+      var player = this;
+      // Delay to start video
+      // setTimeout(function() {
+      //   player.play();
+      // }, 0);
+      player.controlBar.progressControl.disable();
+    });
+    // player.markers({
+    //   markers: [],
+    //   markerStyle: {
+    //     'width': '4px',
+    //     'background-color': 'red'
+    //   }
+    // });
+    this.player = player;
 
+    // Event on space bar key press
+    // document.body.onkeyup = function(e){
+    //   if(e.keyCode == 32){
+    //     e.preventDefault();
+    //     var time = player.currentTime();
+    //     player.markers.add([{ time: time, text: 'hi'}]);
+    //     exp.times.push(time);
+    //   }
+    // }
+  },
+
+  present_handle : function(stim) {
+    $(".err").hide();
+    this.stim = stim; // store this information in the slide so you can record it later
+    // Display sentence
+    $(".svc-sentence").html(stim.svc);
+    $(".cc-sentence").html(stim.cc);
+    // Set src
+    this.player.src({src: stim.roi, type: 'video/mp4'});
+    // Reset markers
+    // this.player.markers.reset([]);
+    // exp.times = [];
+    // Start playing
+    this.player.play();
+  },
+
+  button : function() {
+    // if (exp.sliderPost == null) {
+    //   $(".err").show();
+    // } else {
+    this.log_responses();
+
+    /* use _stream.apply(this); if and only if there is
+    "present" data. (and only *after* responses are logged) */
+    _stream.apply(this);
+    //}
+  },
+
+
+  log_responses : function() {
+    exp.data_trials.push({
+        "src" : this.stim.src,
+        "description": this.stim.sentence,
+        "response" : exp.times//this.player.markers.getMarkers()
+    });
+
+  }
+});
+
+
+
+////////////////////////////////////////////////////////////////////////////////
   slides.subj_info =  slide({
     name : "subj_info",
     submit : function(e){
@@ -210,11 +303,30 @@ function make_slides(f) {
 function init() {
   exp.trials = [];
   exp.catch_trials = [];
+  exp.nQs = 40;
 
   exp.videos = [
-    {sentence: "Fufuo", src: "data/fufu2.mp4"},
-    {sentence: "Market", src: "data/market2.mp4"}
+    {
+      sentence: "Fufuo",
+      src: "data/fufu2.mp4",
+      roi: "data/fufu2_roi.mp4",
+      svc: "SVC sentence",
+      cc: "CC sentence"
+    },
+    {
+      sentence: "Market",
+      src: "data/market2.mp4",
+      roi: "data/market2_roi.mp4",
+      svc: "SVC sentence",
+      cc: "CC sentence"
+    }
   ];
+  exp.videos1 = exp.videos.map(function(video) {
+    return {sentence: video.sentence, src: video.src};
+  });
+  exp.videos2 = exp.videos.map(function(video) {
+    return {roi: video.roi, svc: video.svc, cc: video.cc};
+  });
   exp.times = []; // used as temporary storage
 
   exp.system = {
@@ -233,6 +345,7 @@ function init() {
     //"instructions",
     "practice",
     "one_slider",
+    "preference_slide",
     'subj_info',
     'thanks'
   ];
